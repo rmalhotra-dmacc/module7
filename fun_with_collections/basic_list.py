6bb059eb2c50aa4b9273f3bc39f58e7e0dc20f38
Rajiv Malhotra <malhotra.rajiv@principal.com>
2020-10-07 12:55:45 -0500

    Basic List Assignment - added logic to pass test.

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -8,13 +8,24 @@ Basic List Assignment
 
 
 def make_list():
-    pass
+    u_list = []
+    for i in range(0, 3):
+        try:
+            u_input = int(get_input())
+            u_list.insert(i, u_input)
+        except ValueError:
+            raise ValueError
+    return u_list
 
 
 def get_input():
-    pass
+    u_input = input("Give me a number: ")
+    return u_input
 
 
 if __name__ == '__main__':
-    test_list = make_list()
-    print(test_list)
+    try:
+        test_list = make_list()
+        print(test_list)
+    except ValueError as err:
+        print("Please Enter only numbers")
